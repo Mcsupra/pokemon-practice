c5f8648ea28459e6720a3c1ea044f3633e5cae75
Michael Zide <michaelczide@gmail.com>
2021-02-17 15:00:54 -0600

    Init Commit

---
+++ src/main/java/Pokemon/PokeApiInterfaceApplication.java
@@ -0,0 +1,13 @@
+package Pokemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeApiInterfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeApiInterfaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Pokemon/PokeApiInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package Pokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeApiInterfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
